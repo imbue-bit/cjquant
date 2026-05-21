9492e15bc5b8584c253f168c134238692361d782
栀染 <imbue2025@outlook.com>
2026-05-21 14:37:36 +0000

    暴露为 Python 包

---
+++ cjquant/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1.0"
+
+from .data.pipeline import data_api
+
+from .backtest.engine import OTCBacktestEngine
+from .backtest.slippage import SquareRootSlippage
+
+from .optimizer.traditional import RiskParityOptimizer, MeanVarianceOptimizer
+from .optimizer.machine_learning import HRPOptimizer
+
+from .look_through.engine import LookThroughAnalyzer
+
+from .visualizer.reporter import CJQuantReporter
